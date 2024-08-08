d43ff0758ea6376c73361b9d46e5bd355138758e
王凯 <wangk-ap@glodon.com>
2024-08-08 18:30:02 +0800

    feat: 节流

--- throttle.js
+++ throttle.js
@@ -1,8 +1,8 @@
 /**
  * setTimeout节流
  * 只有第一次被执行
- * @param {function} fn 
- * @param {number} delay 
+ * @param {function} fn
+ * @param {number} delay
  * @returns function
  */
 //  function throttle(fn, delay) {
@@ -14,12 +14,10 @@
 //       timer = null;
 //     })
 //   }
-// } 
+// }
 
 function test(a) {
-  setTimeout(() => {
-    console.log(a);
-  }, 50) 
+  console.log(a);
 }
 // const func = throttle(test, 300);
 
@@ -27,41 +25,38 @@ function test(a) {
 // func(2);
 // func(3);
 
-// function throttleNotSetTimeout(fn, delay) {
-//   let date = new Date();
+function throttleNotSetTimeout(fn, delay) {
+  let date = new Date();
+  return function () {
+    const newDate = new Date();
+    if (newDate - date >= delay) {
+      fn(...arguments);
+      date = newDate;
+    }
+  };
+}
+
+const func = throttleNotSetTimeout(test, 1);
+
+func(1);
+func(2);
+func(3);
+
+// function throttle(fn, delay, immediate = false) {
+//   let timer = null;
 //   return function() {
-//     const newDate = new Date();
-//     console.log(newDate - date >= delay)
-//     if (newDate - date >= delay) {
+//     if (timer) return;
+//     timer = setTimeout(() => {
 //       fn(...arguments);
-//       date = newDate;
-//     }
+//       timer = null;
+//     }, delay)
 //   }
 // }
 
-// const func = throttleNotSetTimeout(test, 1);
+// const func = throttle(test, 200, true);
 
-// func(1);
-// func(2);
-// func(3);
+// func(1)
 
+// func(2)
 
-function throttle(fn, delay, immediate = false) {
-  let timer = null;
-  return function() {
-    if (timer) return;
-    timer = setTimeout(() => {
-      fn(...arguments);
-      timer = null;
-    }, delay)
-  }
-}
-
-const func = throttle(test, 200, true);
-
-
-func(1)
-
-func(2)
-
-func(3)
+// func(3)
