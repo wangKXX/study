66603359bbd3872c6bbed927ceb93f10ce8b67a4
王凯 <wangk-ap@glodon.com>
2024-08-26 10:33:05 +0800

    feat: eslint

--- 手写/new.js
+++ 手写/new.js
@@ -1,5 +1,5 @@
 function myNew(obj, ...args) {
-  const temp = Object.create(obj.property);
+  var temp = Object.create(obj.property);
   const res = temp.apply(obj, args);
   return typeof res === "object" && res !== null ? res : temp;
 }
